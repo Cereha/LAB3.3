602fbde2507eb38b82f0aac8da658200a877da09
Koroban Olya <koroban.olya@gmail.com>
2021-05-06 13:41:29 +0300

    Добавьте файлы проекта.

---
+++ LAB3.3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LAB3._3
+{
+    class Program
+    {
+        private static Semaphore semaphore = new Semaphore(3, 3, "LAB3._3");
+        static void Main(string[] args)
+        {
+            if (_semaphor.WaitOne(1000))
+            {
+                Console.WriteLine("IM Alive");
+                Console.WriteLine("Press any key");
+                Console.ReadKey();
+                _semaphore.Release();
+                _semaphore.Dispose();
+            }
+            else
+            {
+                Console.WriteLine("Cannot obtain semaphore");
+            }
+        }
+    }
+}
