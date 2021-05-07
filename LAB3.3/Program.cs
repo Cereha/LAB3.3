a2a70bdc7c551b442a07b51a5310932631684f00
Koroban Olya <79316653+Cereha@users.noreply.github.com>
2021-05-07 15:15:59 +0300

    Update Program.cs

--- LAB3.3/Program.cs
+++ LAB3.3/Program.cs
@@ -1,13 +1,15 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Threading;
 
 namespace LAB3._3
 {
     class Program
     {
-        private static Semaphore semaphore = new Semaphore(3, 3, "LAB3._3");
+        private static Semaphore _semaphore = new Semaphore(3, 3, "LAB3._3");
         static void Main(string[] args)
         {
-            if (_semaphor.WaitOne(1000))
+            if (_semaphore.WaitOne(1000))
             {
                 Console.WriteLine("IM Alive");
                 Console.WriteLine("Press any key");
